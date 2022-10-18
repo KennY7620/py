56a860c32cd980271a13485b795b8eba99929955
KennY7620 <116047427+KennY7620@users.noreply.github.com>
2022-10-18 04:59:21 +0300

    Add files via upload

---
+++ 21ticket.py
@@ -0,0 +1,7 @@
+import math
+deg = int(input("Введите величину угла в градусах: "))
+rad = deg * math.pi / 180
+print(rad)
+print("cos = ", math.cos(rad))
+print("sin = ", math.sin(rad))
+print("tan = ", math.tan(rad))
